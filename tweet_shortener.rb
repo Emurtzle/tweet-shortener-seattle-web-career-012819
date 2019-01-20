fcf955984170069788b2bc70affb2ffc5932888b
Emily Wagner <emilywagner598@gmail.com>
2019-01-20 04:41:39 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+def word_substituter (string)
+  short_list = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+    }
+
+    new_array = []
+
+    array = string.split.each do |word|
+      changed = false
+      short_list.keys.each do |key|
+        if word == key
+          new_array << short_list[word]
+          changed = true
+          break
+        end
+      end
+      if changed == false
+        new_array << word
+      end
+    end
+    new_array.flatten.join(" ")
+end
+
+def bulk_tweet_shortener (array)
+  array.each do |string|
+    puts word_substituter(string)
+  end
+end
